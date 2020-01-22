543d04cba52c07c7d1bcc5a368ded632b75075fe
BASEL8 <basel84mn@gmail.com>
2020-01-22 11:40:53 +0100

    add user profile tabs

--- src/components/user/UserProfile.jsx
+++ src/components/user/UserProfile.jsx
@@ -2,26 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom'
 import { isAuth, getCookie } from '../../actions/auth'
 import { getUserProfile } from '../../actions/userAuth'
-
+import Main from './profile/main'
 const UserProfile = () => {
   const history = useHistory()
   const { _id } = useParams()
-  const [user, setUser] = useState({ error: '', })
+  const [user, setUser] = useState(isAuth())
   useEffect(() => {
     if (!isAuth()) {
       return history.push('/')
     }
-    // getUserProfile(getCookie('token'), isAuth()._id).then(res => {
-    //   if (res.error) {
-    //     return setUser({ ...user, error: res.error })
-    //   }
-    //   setUser({ ...res, error: '' })
-    // })
-    // eslint-disable-next-line
-    console.log(isAuth())
   }, [history])
   return (
-    <p> user main profile</p>
+    <Main user={user} />
   )
 }
 export default UserProfile;
---
+++ src/components/user/profile/main.jsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import DoneOutlinedIcon from '@material-ui/icons/DoneOutlined';
+import DoneOutlineIcon from '@material-ui/icons/DoneOutline';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import SearchIcon from '@material-ui/icons/Search';
+import BlockIcon from '@material-ui/icons/Block';
+import BlockOutlinedIcon from '@material-ui/icons/BlockOutlined';
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    minHeight: '90vh',
+    '& .MuiTabs-root': {
+      border: 'none',
+    },
+    '& .MuiTabs-flexContainer': {
+      background: theme.palette.primary.main,
+      padding: '20px 10px',
+      borderRadius: 10,
+      margin: 20,
+      width: 60,
+      height: '90%',
+      justifyContent: 'center'
+    },
+    '& .MuiTabs-scroller': {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    '& .MuiTabs-indicator': {
+      width: '50%',
+      borderTopLeftRadius: '100%',
+      borderBottomLeftRadius: '100%',
+      background: 'white',
+      right: 18,
+      '&:before': {
+        zIndex: 999,
+        content: '""',
+        position: 'absolute',
+        top: -32,
+        bottom: 0,
+        right: 0,
+        width: 40,
+        height: 34,
+        borderRadius: '50%',
+        boxShadow: '17px 16px 4px 0 white'
+      },
+      '&:after': {
+        zIndex: 999,
+        content: '""',
+        position: 'absolute',
+        bottom: -32,
+        right: 0,
+        width: 40,
+        height: 34,
+        borderRadius: '50%',
+        boxShadow: '17px -16px 4px 0 white'
+      }
+    },
+    '& .MuiButtonBase-root': {
+      minWidth: 'unset',
+      zIndex: 9999
+    }
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+    zIndex: 999
+  },
+}));
+
+const Main = () => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <div className={classes.root}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        className={classes.tabs}
+      >
+        <Tab label={value !== 0 ? <AccountCircleOutlinedIcon fontSize='small' style={{ color: 'white', opacity: 1 }} /> : <AccountCircleIcon fontSize='small' color={"primary"} />} {...a11yProps(0)} />
+        <Tab label={value !== 1 ? <DoneOutlineIcon fontSize='small' style={{ color: 'white', opacity: 1 }} /> : <DoneOutlinedIcon fontSize='small' color={"primary"} />} {...a11yProps(1)} />
+        <Tab label={value !== 2 ? <SearchOutlinedIcon fontSize='small' style={{ color: 'white', opacity: 1 }} /> : <SearchIcon fontSize='small' color={"primary"} />} {...a11yProps(2)} />
+        <Tab label={value !== 3 ? <BlockOutlinedIcon fontSize='small' style={{ color: 'white', opacity: 1 }} /> : <BlockIcon fontSize='small' color={"primary"} />} {...a11yProps(2)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        user profile
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        contacted by
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        search
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        blocked
+      </TabPanel>
+    </div>
+  )
+}
+
+export default Main;
